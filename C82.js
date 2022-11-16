614ed0fe8e6ef1534ebe9948a561bb19f8852b63
Ddanilo122 <112113175+Ddanilo122@users.noreply.github.com>
2022-11-16 16:25:48 -0300

    Add files via upload

---
+++ C82.js
@@ -0,0 +1,52 @@
+console.log("Meu primeiro projeto no console");
+
+            var firstName = "Joao";
+            var lastName = "Zidane";
+
+
+firstName.concat(lastName)
+            var fullName = lastName ;
+            console.log(fullName);
+
+            var a = 10;
+            var b = 5;
+
+
+ var add = a + b
+
+ console.log(add)
+
+            var add = fullName  ;
+            console.log(add );
+
+
+
+
+ var sub = a - b
+
+console.log(sub)
+
+
+
+            var sub = fullName  ;
+            console.log(sub );
+
+
+
+
+var multiple = a * b
+console.log(multiple)
+
+            var multiple = fullName  ;
+            console.log(multiple);
+
+
+
+            var divide = a / b ;
+            console.log(divide)
+
+
+            var divide = fullName
+            console.log(divide);
+
+            
